8a6a5baeafd70842b36675de4a1d5129a9592a18
quazatic <krypticquasar@gmail.com>
2024-09-06 13:37:17 +1000

    second commit

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
+//Create scene and camera
+const w = window.innerWidth;
+const h = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75,w/h,0.1,1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+//Renderer for the scene
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(w,h);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//Geomoetry and mesh for the object
+const geometry = new THREE.IcosahedronGeometry();
+const material = new THREE.MeshStandardMaterial({color:0x9FC5E8});
+const earthMesh = new THREE.Mesh(geometry,material);
+scene.add(earthMesh);
 
+
+//Camera controls and postion
 camera.position.z = 5;
+const controls = new OrbitControls( camera, renderer.domElement );
 
+//Lighting shader for scene
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+scene.add( light );
+
+//Animation and scene for WEBGL
 function animate() {
-    // Update cube rotation inside the animation loop
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // Update earthMesh rotation inside the animation loop
+    earthMesh.rotation.x += 0.001;
+    earthMesh.rotation.y += 0.002;
+
+    // Update controls
+    controls.update();
   
     // Render the scene and camera on each frame
-    renderer.render( scene, camera );
+    renderer.render(scene, camera,);
   }
   
   // Set the animation loop, which calls animate() on each frame
